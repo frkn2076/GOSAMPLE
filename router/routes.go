2346872a3dac3710cc4e611aba9dcb985270e4be
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-06-26 12:47:10 +0300

    router added

---
+++ router/routes.go
@@ -0,0 +1,31 @@
+package router
+
+import (
+	"app/GoSample/controllers"
+	"app/GoSample/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+	router.Use(middleware.ServiceLogMiddleware())
+
+	accountController := new(controllers.AccountController)
+	heartBeatController := new(controllers.HeartBeatController)
+
+	grp1 := router.Group("/account")
+	{
+		grp1.POST("login", accountController.Login)
+		grp1.POST("register", accountController.Register)
+	}
+
+	grp2 := router.Group("/heartbeat")
+	{
+		grp2.GET("/reports", heartBeatController.GetAllReports)
+		grp2.GET("/clearCache", heartBeatController.ClearCache)
+		grp2.GET("/logs", heartBeatController.GetLogs)
+	}
+
+	return router
+}
