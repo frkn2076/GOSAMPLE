b2055bb9a47251b41571e995b75e9dcf19a19853
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-06-27 11:05:48 +0300

    errors designed

--- infra/customeError/customeErrors.go
+++ infra/customeError/customeErrors.go
@@ -1,17 +1,17 @@
 package customeError
 
+import (
+	"errors"
+)
+
 var UserNotFound error
 var UserAlreadyExists error
 var WrongCredentials error
-var RequestBodyIsNotValid error
 var SomethingWentWrong error
 
 func init() {
-	UserNotFound = New(101, "UserNotFound-Err")
-	UserAlreadyExists = New(102, "UserAlreadyExists-Err")
-	WrongCredentials = New(103, "WrongCredentials-Err")
-
-	RequestBodyIsNotValid = New(201, "SomethingWentWrong-Err")
-
-	SomethingWentWrong = New(999, "SomethingWentWrong-Err")
+	UserNotFound = errors.New("UserNotFound-Err")
+	UserAlreadyExists = errors.New("UserAlreadyExists-Err")
+	WrongCredentials = errors.New("WrongCredentials-Err")
+	SomethingWentWrong = errors.New("SomethingWentWrong-Err")
 }
