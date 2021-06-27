73a24880439786ce81d91040c28b4a94c90c95f6
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-06-27 23:26:03 +0300

    new custome errors added

--- infra/customeError/customeErrors.go
+++ infra/customeError/customeErrors.go
@@ -8,10 +8,18 @@ var UserNotFound error
 var UserAlreadyExists error
 var WrongCredentials error
 var SomethingWentWrong error
+var TodoNotFound error
+var TodoCouldntCreate error
+var TodoCouldntDelete error
+var TodoCouldntUpdate error
 
 func init() {
 	UserNotFound = errors.New("UserNotFound-Err")
 	UserAlreadyExists = errors.New("UserAlreadyExists-Err")
 	WrongCredentials = errors.New("WrongCredentials-Err")
 	SomethingWentWrong = errors.New("SomethingWentWrong-Err")
+	TodoNotFound = errors.New("TodoNotFound-Err")
+	TodoCouldntCreate = errors.New("TodoCouldntCreate-Err")
+	TodoCouldntDelete = errors.New("TodoCouldntDelete-Err")
+	TodoCouldntUpdate = errors.New("TodoCouldntUpdate-Err")
 }
