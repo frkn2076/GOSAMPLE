da12d3095ea2143f58f511a3a6c7e8c5c4e87526
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-06-27 23:25:39 +0300

    todo services impl.

--- controllers/todoController.go
+++ controllers/todoController.go
@@ -35,7 +35,10 @@ func (u *TodoController) AddItem(context *gin.Context) {
 		IsCompleted: todoRequest.IsCompleted}
 
 	transaction := repo.BeginTransaction()
-	repo.Todo.Create(transaction, todo)
+	if isSuccess := repo.Todo.Create(transaction, todo); !isSuccess{
+		context.Error(customeError.TodoCouldntCreate)
+		return
+	}
 	transaction.Commit()
 
 	context.JSON(200, response.Success)
@@ -43,19 +46,74 @@ func (u *TodoController) AddItem(context *gin.Context) {
 
 func (u *TodoController) GetAllItems(context *gin.Context) {
 	userId := context.GetString("userId")
-	todoRecords, err := repo.Todo.GetAll(userId)
-	if err != nil {
+	todoRecords, isSuccess := repo.Todo.GetAll(userId)
+	if !isSuccess {
 		context.Error(customeError.SomethingWentWrong)
 		return
 	}
 
 	var todos []response.TodoModelResponse
 	for _, todoRecord := range todoRecords {
-		todo := response.TodoModelResponse{Name: todoRecord.Name, Description: todoRecord.Description, Deadline: todoRecord.Deadline,
-			 IsCompleted: todoRecord.IsCompleted}
+		todo := response.TodoModelResponse{Id: todoRecord.Id, Name: todoRecord.Name, Description: todoRecord.Description,
+			 Deadline: todoRecord.Deadline, IsCompleted: todoRecord.IsCompleted}
 		todos = append(todos, todo)
 	}
 
 	context.JSON(200, response.TodosResponse{Todos: todos})
+}
+
+func (u *TodoController) DeleteItem(context *gin.Context) {
+	todoIdValue := context.Param("todoId")
+	todoId, err := strconv.ParseUint(todoIdValue, 10, 32)
+	if err != nil {
+		logger.ErrorLog("An error occured while converting string to uint - Value:", todoIdValue, "- Error:", err.Error())
+		context.Error(customeError.SomethingWentWrong)
+		return
+	}
+
+	userIdValue := context.GetString("userId")
+	userId, err := strconv.ParseUint(userIdValue, 10, 32)
+	if err != nil {
+		logger.ErrorLog("An error occured while converting string to uint - Value:", userIdValue, "- Error:", err.Error())
+		context.Error(customeError.SomethingWentWrong)
+		return
+	}
+
+	transaction := repo.BeginTransaction()
+	if isSuccess := repo.Todo.Delete(transaction, uint(todoId), uint(userId)); !isSuccess {
+		context.Error(customeError.TodoCouldntDelete)
+		return
+	}
+	transaction.Commit()
+
+	context.JSON(200, response.Success)
+}
+
+func (u *TodoController) UpdateItem(context *gin.Context) {
+	var todoRequest request.TodoRequest
+	if isSuccess := helper.BindRequest(context, &todoRequest); !isSuccess{
+		context.Error(customeError.SomethingWentWrong)
+		return
+	}
+
+	userIdValue := context.GetString("userId")
+	userId, err := strconv.ParseUint(userIdValue, 10, 32)
+	if err != nil {
+		logger.ErrorLog("An error occured while converting string to uint - Value:", userIdValue, "- Error:", err.Error())
+		context.Error(customeError.SomethingWentWrong)
+		return
+	}
+
+	todo := entities.Todo{Id: todoRequest.Id, UserId: uint(userId),  Name: todoRequest.Name, Description: todoRequest.Description,
+		 Deadline: todoRequest.Deadline, IsCompleted: todoRequest.IsCompleted}
+
+	transaction := repo.BeginTransaction()
+	if isSuccess := repo.Todo.Update(transaction, todo); !isSuccess {
+		context.Error(customeError.TodoCouldntUpdate)
+		return
+	}
+	transaction.Commit()
+
+	context.JSON(200, response.Success)
 }
 
