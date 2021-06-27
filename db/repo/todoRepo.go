b1bb4c3d86bfe2bc1f3ccfae89b0ef73ff8d12df
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-06-27 23:25:47 +0300

    todo repo impl.

--- db/repo/todoRepo.go
+++ db/repo/todoRepo.go
@@ -18,32 +18,45 @@ func init() {
 
 type TodoRepo struct{}
 
-func (u *TodoRepo) Create(db *gorm.DB, todo entities.Todo) {
+func (u *TodoRepo) Create(db *gorm.DB, todo entities.Todo) bool {
 	if err := db.Create(&todo).Error; err != nil {
 		logger.ErrorLog("An error occured while creating todo - todoRepo.go - Todo:", todo, "- Error:", err.Error())
+		return false
 	}
+	return true
 }
 
-func (u *TodoRepo) Delete(db *gorm.DB, id int) {
-	if err := db.Where("id = ?", id).Delete(&entities.Todo{}).Error; err != nil {
+func (u *TodoRepo) Delete(db *gorm.DB, id uint, userId uint) bool {
+	if err := db.Where("id = ? and user_id = ?", id, userId).Delete(&entities.Todo{}).Error; err != nil {
 		errorMessage := fmt.Sprintf("An error occured while deleting todo - todoRepo.go - id = %d - Error: %s", id, err.Error())
 		logger.ErrorLog(errorMessage)
+		return false
 	}
+	return true
 }
 
-func (u *TodoRepo) UpdateIsCompleted(db *gorm.DB, id int, isCompleted bool) {
-	if err := db.Model(&entities.Todo{}).Where("id = ?", id).Update("is_completed", isCompleted).Error; err != nil {
-		errorMessage := fmt.Sprintf("An error occured while updating todo - todoRepo.go - id = %d, isCompleted = %t - Error: %s", id, isCompleted, err.Error())
+func (u *TodoRepo) Update(db *gorm.DB, todo entities.Todo) bool {
+	update := map[string]interface{}{
+		"name": todo.Name,
+		"description": todo.Description,
+		"deadline": todo.Deadline,
+		"is_completed": todo.IsCompleted,
+	}
+
+	if err := db.Model(&entities.Todo{}).Where("id = ? and user_id = ?", todo.Id, todo.UserId).Updates(update).Error; err != nil {
+		errorMessage := fmt.Sprintf("An error occured while updating todo - todoRepo.go - Error: %s", err.Error())
 		logger.ErrorLog(errorMessage)
+		return false
 	}
+	return true
 }
 
-func (u *TodoRepo) GetAll(userId string) ([]entities.Todo, error) {
+func (u *TodoRepo) GetAll(userId string) ([]entities.Todo, bool) {
 	var todos []entities.Todo
 	if err := database.GormDB.Where("user_id = ?", userId).Find(&todos).Error; err != nil {
 		errorMessage := fmt.Sprintf("An error occured while getting all todos - todoRepo.go - Error: %s", err.Error())
 		logger.ErrorLog(errorMessage)
-		return todos, err
+		return todos, false
 	}
-	return todos, nil
+	return todos, true
 }
