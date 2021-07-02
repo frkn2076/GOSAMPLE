4818d3f12ad9bc7fd4b92b132f80782c56234f9c
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-07-02 12:53:34 +0300

    env loader issue fixed

--- config/environments/environment.go
+++ config/environments/environment.go
@@ -8,10 +8,10 @@ import (
 )
 
 func init() {
-	environment := os.Args[1]
-	if environment != "PROD" && environment != "STAGE" && environment != "DEV" {
-		environment = "LOCAL-DEFAULT"
-	}
+	environment := "LOCAL-DEFAULT"
+	if len(os.Args) > 1 {
+		environment = os.Args[1]
+	} 
 
 	fmt.Println("Environment selected as", environment)
 	envFilePath := fmt.Sprintf("config/environments/%s.env", environment)
