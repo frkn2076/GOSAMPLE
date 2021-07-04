e13313f6e040296d32b71bdeecff6f01464dceae
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-07-04 16:46:33 +0300

    redesigned and added interface

--- controllers/helper/helper.go
+++ controllers/helper/helper.go
@@ -4,6 +4,7 @@ import (
 	"os"
 
 	"app/GoSample/config/session"
+	"app/GoSample/infra/auth"
 	"app/GoSample/infra/constant"
 	"app/GoSample/logger"
 
@@ -12,8 +13,15 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-//#region Request/Response
-func BindRequest(context *gin.Context, model interface{}) bool {
+var HelperInstance *Helper
+
+func init() {
+	HelperInstance = new(Helper)
+}
+
+type Helper struct{}
+
+func (h *Helper) BindRequest(context *gin.Context, model interface{}) bool {
 	if err := context.ShouldBindJSON(&model); err != nil {
 		logger.ErrorLog("Invalid request - helper.go - Model:", model, "- Error:", err.Error())
 		return false
@@ -21,11 +29,8 @@ func BindRequest(context *gin.Context, model interface{}) bool {
 	return true
 }
 
-//#endregion
-
-//#region Session
-func AddToSession(context *gin.Context, key string, value string) bool {
-	session, err := GetSession(context)
+func (h *Helper) AddToSession(context *gin.Context, key string, value string) bool {
+	session, err := h.GetSession(context)
 	if err != nil {
 		return false
 	}
@@ -37,7 +42,7 @@ func AddToSession(context *gin.Context, key string, value string) bool {
 	return true
 }
 
-func GetSession(context *gin.Context) (ses *sessions.Session, err error) {
+func (h *Helper) GetSession(context *gin.Context) (ses *sessions.Session, err error) {
 	sessionCookieName := os.Getenv("SessionCookieName")
 	ses, err = session.Store.Get(context.Request, sessionCookieName)
 	if err != nil {
@@ -46,10 +51,7 @@ func GetSession(context *gin.Context) (ses *sessions.Session, err error) {
 	return
 }
 
-//#endregion
-
-//#region Hashing
-func HashPassword(context *gin.Context, password string) (string, bool) {
+func (h *Helper) HashPassword(context *gin.Context, password string) (string, bool) {
 	passwordBytes := []byte(password)
 	hashedPasswordBytes, err := bcrypt.GenerateFromPassword(passwordBytes, bcrypt.DefaultCost)
 	if err != nil {
@@ -60,9 +62,15 @@ func HashPassword(context *gin.Context, password string) (string, bool) {
 	return hashedPassword, true
 }
 
-func CheckPasswordHash(password, hash string) bool {
+func (h *Helper) CheckPasswordHash(password, hash string) bool {
 	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 	return err == nil
 }
 
-//#endregion
+func (h *Helper) GenerateToken(userId uint) (string, bool) {
+	token, err := auth.JWT.GenerateToken(userId)
+	if err != nil {
+		return constant.EmptyString, false
+	}
+	return token, true
+}
---
+++ controllers/helper/ihelper.go
@@ -0,0 +1,15 @@
+package helper
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/gorilla/sessions"
+)
+
+type IHelper interface {
+	BindRequest(context *gin.Context, model interface{}) bool
+	AddToSession(context *gin.Context, key string, value string) bool
+	GetSession(context *gin.Context) (ses *sessions.Session, err error)
+	HashPassword(context *gin.Context, password string) (string, bool)
+	CheckPasswordHash(password, hash string) bool
+	GenerateToken(userId uint) (string, bool)
+}
