40459f6656e003541faf42343c50042c9525f3c1
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-07-04 20:42:16 +0300

    mocks of todo repository added

--- test/mocks/mockTodoRepo.go
+++ test/mocks/mockTodoRepo.go
@@ -24,8 +24,8 @@ func (e MockTodoRepo) Update(db *gorm.DB, todo entities.Todo) bool {
 
 func (e MockTodoRepo) GetAll(userId string) ([]entities.Todo, bool) {
 	return []entities.Todo{
-		entities.Todo{ UserId: 1, Name: "DummyName1", Description: "DummyDescription1", Deadline: time.Now(), IsCompleted: true },
-		entities.Todo{ UserId: 1, Name: "DummyName2", Description: "DummyDescription2", Deadline: time.Now(), IsCompleted: false },
+		entities.Todo{ UserId: 1, Name: "DummyName1", Description: "DummyDescription1", Deadline: time.Date(1999, time.January, 03, 0, 0, 0, 0, time.UTC), IsCompleted: true },
+		entities.Todo{ UserId: 1, Name: "DummyName2", Description: "DummyDescription2", Deadline: time.Date(1999, time.January, 03, 0, 0, 0, 0, time.UTC), IsCompleted: false },
 	}, true
 }
 
@@ -50,3 +50,63 @@ func (e MockCreateTodoFailTodoRepo) GetAll(userId string) ([]entities.Todo, bool
 	}, true
 }
 
+type MockGetAllFailTodoRepo struct{}
+
+func (e MockGetAllFailTodoRepo) Create(db *gorm.DB, todo entities.Todo) bool {
+	return true
+}
+
+func (e MockGetAllFailTodoRepo) Delete(db *gorm.DB, id uint, userId uint) bool {
+	return true
+}
+
+func (e MockGetAllFailTodoRepo) Update(db *gorm.DB, todo entities.Todo) bool {
+	return true
+}
+
+func (e MockGetAllFailTodoRepo) GetAll(userId string) ([]entities.Todo, bool) {
+	return nil, false
+}
+
+type MockUpdateFailTodoRepo struct{}
+
+func (e MockUpdateFailTodoRepo) Create(db *gorm.DB, todo entities.Todo) bool {
+	return true
+}
+
+func (e MockUpdateFailTodoRepo) Delete(db *gorm.DB, id uint, userId uint) bool {
+	return true
+}
+
+func (e MockUpdateFailTodoRepo) Update(db *gorm.DB, todo entities.Todo) bool {
+	return false
+}
+
+func (e MockUpdateFailTodoRepo) GetAll(userId string) ([]entities.Todo, bool) {
+	return []entities.Todo{
+		entities.Todo{ UserId: 1, Name: "DummyName1", Description: "DummyDescription1", Deadline: time.Date(1999, time.January, 03, 0, 0, 0, 0, time.UTC), IsCompleted: true },
+		entities.Todo{ UserId: 1, Name: "DummyName2", Description: "DummyDescription2", Deadline: time.Date(1999, time.January, 03, 0, 0, 0, 0, time.UTC), IsCompleted: false },
+	}, true
+}
+
+
+type MockDeleteFailTodoRepo struct{}
+
+func (e MockDeleteFailTodoRepo) Create(db *gorm.DB, todo entities.Todo) bool {
+	return true
+}
+
+func (e MockDeleteFailTodoRepo) Delete(db *gorm.DB, id uint, userId uint) bool {
+	return false
+}
+
+func (e MockDeleteFailTodoRepo) Update(db *gorm.DB, todo entities.Todo) bool {
+	return true
+}
+
+func (e MockDeleteFailTodoRepo) GetAll(userId string) ([]entities.Todo, bool) {
+	return []entities.Todo{
+		entities.Todo{ UserId: 1, Name: "DummyName1", Description: "DummyDescription1", Deadline: time.Date(1999, time.January, 03, 0, 0, 0, 0, time.UTC), IsCompleted: true },
+		entities.Todo{ UserId: 1, Name: "DummyName2", Description: "DummyDescription2", Deadline: time.Date(1999, time.January, 03, 0, 0, 0, 0, time.UTC), IsCompleted: false },
+	}, true
+}
